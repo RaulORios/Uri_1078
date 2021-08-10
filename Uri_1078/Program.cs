035a17ee697358dc1b0b6c394e28345ff1881913
RaulORios <raul.orios89@outlook.com>
2021-08-10 14:12:51 -0300

    Arquivos adicionados

---
+++ Uri_1078/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uri_1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int tabuada = i * n;
+                Console.WriteLine($"{i} x {n} = {tabuada}");
+            }
+        }
+    }
+}
